d6059d1fcd47d0f2e70dfd08b0616b81bbb00ab2
Thibaut Foussard <thibaut.foussard@tmrw.com>
2022-05-11 15:58:31 -0400

    removed unnecessary code from main.js + css easing update

--- frontend/assets/scripts/main.js
+++ frontend/assets/scripts/main.js
@@ -1,12 +1,4 @@
 import '../styles/styles.scss';
 import App from './app';
 
-const init = () => {
-    App.init();
-};
-
-window.onbeforeunload = () => {
-    window.scrollTo(0, 0);
-};
-
-document.addEventListener('DOMContentLoaded', () => init());
+App.init();
